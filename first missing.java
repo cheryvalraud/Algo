4601d18400e588d088059fb526999c098fc5af51
cheryvalraud <127906312+cheryvalraud@users.noreply.github.com>
2024-12-01 18:13:20 -0500

    Update first missing.java

--- first missing.java
+++ first missing.java
@@ -1,40 +1,30 @@
-import java.util.*;
+// Leetcode form
 
-class Main {
-    public static void main(String[] args) {
-        
-                int[] nums = {1, 2, 0, 4, 5};
-                
-    List<Integer> missingNumber = findMissingNumbers(nums);
-    
-     System.out.println("Missing numbers: " + missingNumber);
-        
-    }
-    
-    
-    public static List<Integer> findMissingNumbers(int[]nums){
-           int n = nums.length;
-        List<Integer> missingNumbers = new ArrayList<>();
-        for(int i = 0; i < n ; i++){
-            
-            if(nums[i] <= 0 || nums[i] > n){
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0 || nums[i] > n) {
                 nums[i] = n + 1;
             }
         }
-        
-        for(int i = 0; i < n ; i++){
+
+        for (int i = 0; i < n; i++) {
             int val = Math.abs(nums[i]);
-            if(val <= n){
-            nums[val - 1] = -Math.abs(nums[val - 1]); 
+            if (val <= n) {
+                if (nums[val - 1] > 0) {
+                    nums[val - 1] = -nums[val - 1];
+                }
             }
         }
-        
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] > 0){
-                missingNumbers.add(i + 1);
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
             }
         }
-        
-        return missingNumbers;
+
+        return n + 1;
     }
 }
