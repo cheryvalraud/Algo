f5f9be225f36f6f0705874ed31772f20de6eaa99
cheryvalraud <127906312+cheryvalraud@users.noreply.github.com>
2024-12-06 15:04:54 -0500

    Create problem2.java

---
+++ problem2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        
+        String message = "Class starts at 10" ;
+        
+        int k = 3 ;
+        int i = 0;
+        
+     public static String removing(String message){
+         if(message == " ") retrun " ";
+         
+         
+         return removing(message.substring(0)) + message.replace.(message.CharAt(3));
+     }
+        
+    }
+}
